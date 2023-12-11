f312a7f074c217fedd800d9db1d8b5561efd9345
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-12-11 16:00:22 +0200

    refactored

--- src/components/sceens/page/scenarios/content/FullScenario.jsx
+++ src/components/sceens/page/scenarios/content/FullScenario.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './FullScenario.module.css';
 
-const FullScenario = ({ scenario, setScenario }) => {
+const FullScenario = ({scenario, setScenario}) => {
     const handleClose = () => {
         setScenario(null);
     };
@@ -12,26 +12,38 @@ const FullScenario = ({ scenario, setScenario }) => {
                 <button className={styles.close_button} onClick={handleClose}>
                     x
                 </button>
-                <div className={styles.box_container}>
-                    <div className={styles.scenario_box}>
-                        <div className={styles.scenario_name}>{scenario.name}</div>
+
+                <div className={styles.content}>
+                    <div className={styles.left_box}>
+                        <button>
+                            Button 1
+                        </button>
                     </div>
-                    <div className={styles.scenario_box}>
-                        <div className={styles.scenario_site}>{scenario.site}</div>
-                    </div>
-                </div>
-                <div className={styles.step_list}>
-                    <h2>Steps:</h2>
-                    {scenario.steps.map((step, index) => (
-                        <div key={index} className={styles.step_box}>
-                            <div className={styles.step_action}>
-                                {step.action}
-                            </div>
-                            <div className={styles.step_value}>
-                               {step.value}
+                    <div className={styles.center_box}>
+                        <div className={styles.box_container}>
+                            <div className={styles.scenario_box}>
+                                <div className={styles.scenario_name}>{scenario.name}</div>
+                                <div className={styles.scenario_site}>{scenario.site}</div>
                             </div>
                         </div>
-                    ))}
+                        <div className={styles.step_list}>
+                            <h2>Steps:</h2>
+                            {scenario.steps.map((step, index) => (
+                                <div key={index} className={styles.step_box}>
+                                    <div className={styles.step_action}>
+                                        {step.action}
+                                    </div>
+                                    <div className={styles.step_value}>
+                                        {step.value}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className={styles.right_box}>
+
+                        <h1>gg</h1>
+                    </div>
                 </div>
             </div>
         </div>
