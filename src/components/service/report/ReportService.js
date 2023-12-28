77228f2ed7761d594dc9e89dd94717e37d011274
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-12-29 01:18:50 +0200

    implemented new methods

--- src/components/service/report/ReportService.js
+++ src/components/service/report/ReportService.js
@@ -15,7 +15,43 @@ const ReportService = {
         } catch (error) {
             HandleApiError(error)
         }
-    }
-}
+    },
+
+    findSuccessfulByName: async (name, pageNum, pageSize) => {
+        try {
+            const response = await axios.get(`${BASE_URL}/successful/name=${name}`, { params: { pageNum, pageSize } });
+            return response.data;
+        } catch (error) {
+            HandleApiError(error);
+        }
+    },
+
+    findSuccessfulBySite: async (site, pageNum, pageSize) => {
+        try {
+            const response = await axios.get(`${BASE_URL}/successful/site=${site}`, { params: { pageNum, pageSize } });
+            return response.data;
+        } catch (error) {
+            HandleApiError(error);
+        }
+    },
+
+    findFailedByName: async (name, pageNum, pageSize) => {
+        try {
+            const response = await axios.get(`${BASE_URL}/failed/name=${name}`, { params: { pageNum, pageSize } });
+            return response.data;
+        } catch (error) {
+            HandleApiError(error);
+        }
+    },
+
+    findFailedBySite: async (site, pageNum, pageSize) => {
+        try {
+            const response = await axios.get(`${BASE_URL}/failed/site=${site}`, { params: { pageNum, pageSize } });
+            return response.data;
+        } catch (error) {
+            HandleApiError(error);
+        }
+    },
+};
 
 export default ReportService;
