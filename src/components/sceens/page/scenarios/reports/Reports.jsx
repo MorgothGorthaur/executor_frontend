e2258893ada970c820b802da4e821f6ef6f27eac
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-12-29 00:59:34 +0200

    created css module

--- src/components/sceens/page/scenarios/reports/Reports.jsx
+++ src/components/sceens/page/scenarios/reports/Reports.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ReportService from "../../../../service/report/ReportService.js";
+import saveIcon from "../../../../../assets/save.svg";
+import styles from "./Report.module.css";
 
 
 const Reports = ({id}) => {
@@ -18,12 +20,31 @@ const Reports = ({id}) => {
         fetchReportsById();
     }, [id]);
 
+    const downloadReport = (report) => {
+        const blob = new Blob([JSON.stringify(report)], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'report.json';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+    };
+
     return (
         <div>
-            <h1>Reports</h1>
             <ul>
-                {reports.map((report) => (
-                    <li key={report.id}>{report.name}</li>
+                {reports.map((report, index) => (
+                    <div key={index} className={styles.block}>
+                        <span>{report.errorMessage ? 'failed' : 'successful'}</span>
+                        <button
+                            className={styles.button}
+                            onClick={() => downloadReport(report)}
+                        >
+                            <img src={saveIcon} alt="Download Report"/>
+                        </button>
+                    </div>
                 ))}
             </ul>
         </div>
